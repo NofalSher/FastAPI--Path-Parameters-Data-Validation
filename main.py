80450961857e12373a6fac4e63c795b382de5dda
NofalSher <nofalsher7@gmail.com>
2025-06-16 15:50:17 +0500

    completed validation and path parameters

--- main.py
+++ main.py
@@ -1,15 +1,24 @@
 from fastapi import FastAPI, HTTPException
-
+from enum import Enum
 app= FastAPI()
 
 BANDS = [
     {"id": 1, "name": "The Beatles", "genre": "Rock"},
     {"id": 2, "name": "Nirvana", "genre": "Grunge"},
-    {"id": 3, "name": "Led Zeppelin", "genre": "Rock"},
-    {"id": 4, "name": "The Rolling Stones", "genre": "Rock"}
+    {"id": 3, "name": "Black sem", "genre": "Metal"},
+    {"id": 4, "name": "The Rolling Stones", "genre": "Electronic"},
 ]
 
-
+class url_choices(Enum):
+    Rock = "rock"
+    Grunge = "grunge" 
+    Electronic = "electronic"
+    Metal = "metal"
+    Pop = "pop"
+    HipHop = "hiphop"
+    
+    
+    
 @app.get("/bands")
 async def bands() -> list[dict]:
     return BANDS
@@ -25,8 +34,9 @@ async def band(band_id: int) -> dict:
 
 
 @app.get("/bands/genre/{genre}")
-async def bands_by_genre(genre: str) -> list[dict]:
-    filtered_bands = [band for band in BANDS if band["genre"].lower() == genre.lower()]
+async def bands_by_genre(genre: url_choices) -> list[dict]:
+    # filtered_bands = [band for band in BANDS if band["genre"].lower() == genre.lower()]  # basic
+    filtered_bands = [band for band in BANDS if band["genre"].lower() == genre.value]  # using enum funcitonality
     if not filtered_bands:
         raise HTTPException(status_code=404, detail="No bands found for this genre")
     return filtered_bands
